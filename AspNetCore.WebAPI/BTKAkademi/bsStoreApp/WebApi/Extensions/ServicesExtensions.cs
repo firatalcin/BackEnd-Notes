5708197efd059e76d265546b283e324a16ba2772
Fırat Alçın <firatalcin04@gmail.com>
2023-10-10 19:35:20 +0300

    Service Extensions

---
+++ AspNetCore.WebAPI/BTKAkademi/bsStoreApp/WebApi/Extensions/ServicesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.EFCore;
+using System.Runtime.CompilerServices;
+
+namespace WebApi.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("sqlConnection"));
+            });
+        }
+    }
+}
