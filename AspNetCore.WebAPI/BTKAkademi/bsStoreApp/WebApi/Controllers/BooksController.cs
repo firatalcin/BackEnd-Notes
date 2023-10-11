bffa288224c70a76d3b37c2f903fd1f7f19aeb12
Fırat Alçın <firatalcin04@gmail.com>
2023-10-11 23:52:58 +0300

    Service Manager Kullanımı

--- AspNetCore.WebAPI/BTKAkademi/bsStoreApp/WebApi/Controllers/BooksController.cs
+++ AspNetCore.WebAPI/BTKAkademi/bsStoreApp/WebApi/Controllers/BooksController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Repositories.Contracts;
 using Repositories.EFCore;
+using Services.Contracts;
 
 namespace WebApi.Controllers
 {
@@ -10,9 +11,9 @@ namespace WebApi.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
-        public readonly IRepositoryManager _manager;
+        public readonly IServiceManager _manager;
 
-        public BooksController(IRepositoryManager manager)
+        public BooksController(IServiceManager manager)
         {
             _manager = manager;
         }
@@ -22,7 +23,7 @@ namespace WebApi.Controllers
         {
             try
             {
-                var books = _manager.Book.GetAllBooks(false);
+                var books = _manager.BookService.GetAllBooks(false);
                 return Ok(books);
             }
             catch (Exception ex)
@@ -38,7 +39,7 @@ namespace WebApi.Controllers
             try
             {
                 var book = _manager
-                .Book
+                .BookService
                 .GetOneBookById(id, false);
 
                 if (book is null)
@@ -61,8 +62,8 @@ namespace WebApi.Controllers
                 if (book is null)
                     return BadRequest();
 
-                _manager.Book.CreateOneBook(book);
-                _manager.Save();
+                _manager.BookService.CreateOneBook(book);
+
                 return StatusCode(201, book);
             }
             catch (Exception ex)
@@ -77,21 +78,12 @@ namespace WebApi.Controllers
         {
             try
             {
-                // check book?
-                var entity = _manager.Book.GetOneBookById(id, true);
-
-                if (entity is null)
-                    return NotFound();
-
-                // check id?
-                if (id != book.Id)
+                if(book is null)
                     return BadRequest();
 
-                entity.Title = book.Title;
-                entity.Price = book.Price;
+                _manager.BookService.UpdateOneBook(id, book, true);
 
-                _manager.Save();
-                return Ok(book);
+                return NoContent();
             }
             catch (Exception ex)
             {
@@ -103,29 +95,20 @@ namespace WebApi.Controllers
         [HttpDelete("{id:int}")]
         public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
         {
-            var entity = _manager.Book.GetOneBookById(id, false);
-
-            if (entity is null)
-                return NotFound(new
-                {
-                    StatusCode = 404,
-                    message = $"Book with id: {id} could not found"
-                });
-
-            _manager.Book.DeleteOneBook(entity);
-            _manager.Save();
+            _manager.BookService.DeleteOneBook(id,false);
             return NoContent();
         }
 
         [HttpPatch("{id:int}")]
         public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
         {
-            var entity = _manager.Book.GetOneBookById(id, true);
+            var entity = _manager.BookService.GetOneBookById(id, true);
             if (entity is null)
                 return NotFound();
 
             bookPatch.ApplyTo(entity);
-            _manager.Book.Update(entity);
+            _manager.BookService.UpdateOneBook(id, entity, true);
+
             return NoContent();
         }
     }
