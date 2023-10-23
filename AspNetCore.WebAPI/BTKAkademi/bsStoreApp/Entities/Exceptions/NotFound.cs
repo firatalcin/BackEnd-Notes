ac53cf59bea1cfeef0ee1c7104188a5376b6f1fa
Fırat Alçın <firatalcin04@gmail.com>
2023-10-23 23:02:59 +0300

    Not Found Exception

---
+++ AspNetCore.WebAPI/BTKAkademi/bsStoreApp/Entities/Exceptions/NotFound.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Exceptions
+{
+    public abstract partial class NotFound : Exception
+    {
+        protected NotFound(string message) : base(message)
+        {
+            
+        }
+    }
+}
