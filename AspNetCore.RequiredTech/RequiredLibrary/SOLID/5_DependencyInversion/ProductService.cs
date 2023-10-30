2934d9d71294108971d1f5c411edba4adf633353
Fırat Alçın <firatalcin04@gmail.com>
2023-10-31 01:00:19 +0300

    Dependency Inversion

---
+++ AspNetCore.RequiredTech/RequiredLibrary/SOLID/5_DependencyInversion/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_DependencyInversion
+{
+	public class ProductService
+	{
+		private readonly IRepository _repository;
+
+		public ProductService(IRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public List<string> GetAll()
+		{
+			return _repository.GetAll();
+		}
+	}
+
+	public class ProductRepositoryFromSQLServer : IRepository
+	{
+		public List<string> GetAll()
+		{
+			return new List<string>() { "SqlServer kalem1", "SqlServer kalem2"};
+		}
+	}
+
+	public class ProductRepositoryFromOracle : IRepository
+	{
+		public List<string> GetAll()
+		{
+			return new List<string>() { "Oracle kalem1", "Oracle kalem2" };
+		}
+	}
+
+	public interface IRepository
+	{
+		List<string> GetAll();
+	}
+}
---
+++ AspNetCore.RequiredTech/RequiredLibrary/SOLID/5_DependencyInversion/Program.cs
@@ -0,0 +1,8 @@
+﻿//High level nesnelerin low level nesnelere direkt bağlantısı olmamalıdır.
+
+
+using _5_DependencyInversion;
+
+ProductService productService = new ProductService(new ProductRepositoryFromOracle());
+
+productService.GetAll().ForEach(x => Console.WriteLine(x));
