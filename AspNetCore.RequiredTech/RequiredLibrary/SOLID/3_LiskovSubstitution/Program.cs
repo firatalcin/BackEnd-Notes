8c3417e1b0ff9b00b67b1f9efff920f905477426
Fırat Alçın <firatalcin04@gmail.com>
2023-10-30 23:30:17 +0300

    Liskov Substitution

---
+++ AspNetCore.RequiredTech/RequiredLibrary/SOLID/3_LiskovSubstitution/Program.cs
@@ -0,0 +1,11 @@
+﻿using _3_LiskovSubstitution;
+
+//Miras veren classların özelliklerinin tamamı miras verdiği classlarda da çalışmalı
+
+IPhone phone = new IPhone();
+phone.Call();
+phone.TakePhoto();
+
+Nokia3310 phone2 = new Nokia3310();
+phone2.Call();
+
