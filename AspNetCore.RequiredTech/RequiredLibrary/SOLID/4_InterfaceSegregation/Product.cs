553123a25d4421f022ef4cb77ee971c80009cea3
Fırat Alçın <firatalcin04@gmail.com>
2023-10-31 00:44:32 +0300

    Interfaces Segregation

---
+++ AspNetCore.RequiredTech/RequiredLibrary/SOLID/4_InterfaceSegregation/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_InterfaceSegregation
+{
+	public class Product
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+	public class ProductRead : IProductRepositoryRead
+	{
+		public List<Product> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Product GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class ProductWrite : IProductRepositoryWrite
+	{
+		public Product Create(Product p)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Product Delete(Product p)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Product Update(Product p)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public interface IProductRepositoryRead
+	{
+		List<Product> GetAll();
+		Product GetById(int id);
+	}
+
+	public interface IProductRepositoryWrite
+	{
+		Product Create(Product p);
+		Product Update(Product p);
+		Product Delete(Product p);
+	}
+}
