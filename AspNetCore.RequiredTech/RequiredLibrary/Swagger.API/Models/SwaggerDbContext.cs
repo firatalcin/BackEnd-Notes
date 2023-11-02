803d11d7d35fb996ef029f389007ea9a7e1d4799
Fırat Alçın <firatalcin04@gmail.com>
2023-11-02 23:39:24 +0300

    ProductsController Api Oluşturmak

---
+++ AspNetCore.RequiredTech/RequiredLibrary/Swagger.API/Models/SwaggerDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Swagger.API.Models
+{
+    public partial class SwaggerDbContext : DbContext
+    {
+        public SwaggerDbContext(DbContextOptions<SwaggerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+     
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Category).HasMaxLength(50);
+
+                entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
