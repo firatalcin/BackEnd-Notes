3be3acb17837246ebdcce050b248c1408c72217f
Fırat Alçın <firatalcin04@gmail.com>
2023-11-14 00:46:34 +0300

    Book Repository Extension

--- AspNetCore.WebAPI/BTKAkademi/bsStoreApp/Repositories/EFCore/BookRepository.cs
+++ AspNetCore.WebAPI/BTKAkademi/bsStoreApp/Repositories/EFCore/BookRepository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Repositories.EFCore
 {
-    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    public sealed class BookRepository : RepositoryBase<Book>, IBookRepository
     {
         public BookRepository(RepositoryContext context) : base(context)
         {
@@ -28,7 +28,8 @@ namespace Repositories.EFCore
 
         public async Task<PagedList<Book>> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
         {
-            var books = await FindByCondition(b => (b.Price >= bookParameters.MinPrice) && (b.Price <= bookParameters.MaxPrice),  trackChanges)
+            var books = await FindAll(trackChanges)
+                .FilterBooks(bookParameters.MinPrice, bookParameters.MaxPrice)
                 .OrderBy(x => x.Id)
                 .ToListAsync();
 
