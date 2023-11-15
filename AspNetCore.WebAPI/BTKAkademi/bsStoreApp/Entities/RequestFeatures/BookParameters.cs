a1d47e6f27fd1bba62b7a55884fddfb2cac95160
Fırat Alçın <firatalcin04@gmail.com>
2023-11-15 23:50:43 +0300

    OrderByQueryString

--- AspNetCore.WebAPI/BTKAkademi/bsStoreApp/Entities/RequestFeatures/BookParameters.cs
+++ AspNetCore.WebAPI/BTKAkademi/bsStoreApp/Entities/RequestFeatures/BookParameters.cs
@@ -5,7 +5,10 @@
         public uint MinPrice { get; set; }
         public uint MaxPrice { get; set; } = 1000;
         public bool ValidPriceRange => MaxPrice > MinPrice;
-
         public string? SearchTerm { get; set; }
+        public BookParameters()
+        {
+            OrderBy = "id";
+        }
     }
 }
--- AspNetCore.WebAPI/BTKAkademi/bsStoreApp/Entities/RequestFeatures/RequestParameters.cs
+++ AspNetCore.WebAPI/BTKAkademi/bsStoreApp/Entities/RequestFeatures/RequestParameters.cs
@@ -19,5 +19,7 @@ namespace Entities.RequestFeatures
 			get { return _pageSize; }
 			set { _pageSize = value > maxPageSize ? maxPageSize : value; }
 		}
-	}
+
+        public string? OrderBy { get; set; }
+    }
 }
