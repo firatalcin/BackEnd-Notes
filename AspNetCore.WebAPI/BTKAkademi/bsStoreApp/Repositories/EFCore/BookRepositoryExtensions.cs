14298852f3d4b7b7b0aec9648ad8436ed998f1ca
Fırat Alçın <firatalcin04@gmail.com>
2023-11-16 00:06:27 +0300

    Sort

--- AspNetCore.WebAPI/BTKAkademi/bsStoreApp/Repositories/EFCore/BookRepositoryExtensions.cs
+++ AspNetCore.WebAPI/BTKAkademi/bsStoreApp/Repositories/EFCore/BookRepositoryExtensions.cs
@@ -1,9 +1,7 @@
 ﻿using Entities.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositories.EFCore
 {
@@ -22,6 +20,43 @@ namespace Repositories.EFCore
 			var lowerCaseTerm = searchTerm.Trim().ToLower();
 			return books
 				.Where(x => x.Title.ToLower().Contains(searchTerm));
+		}
+
+		public static IQueryable<Book> Sort(this IQueryable<Book> books, string orderByQueryString)
+		{
+			if(string.IsNullOrWhiteSpace(orderByQueryString))
+				return books.OrderBy(x => x.Id);
+
+			var orderParams = orderByQueryString.Trim().Split(',');
+
+			var propertyInfos = typeof(Book)
+				.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+			var orderQueryBuilder = new StringBuilder();
+
+			foreach(var param in orderParams)
+			{
+				if(string.IsNullOrWhiteSpace(param))
+					continue;
+
+				var propertyFromQueryName = param.Split(' ')[0];
+
+				var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+				if (objectProperty is null)
+					continue;
+
+				var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+				orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
+			}
+
+			var orderQuery = orderQueryBuilder.ToString().TrimEnd(',',' ');
+
+			if (orderQuery is null)
+				return books.OrderBy(x => x.Id);
+
+			return books.OrderBy(orderQuery);
 		}
 	}
 }
