9c0a3c7be77a6856d64bc0b1ab224bfd7cb86d6b
Fırat Alçın <firatalcin04@gmail.com>
2023-11-23 23:36:06 +0300

    Constructor

---
+++ DerinlemesineOOP/OOP1/_6_Constructor/MyClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Constructor
+{
+	public class MyClass
+	{
+
+        /// <summary>
+        /// Bu bir constructor metottur
+        /// </summary>
+        public MyClass(int a) { 
+            Console.WriteLine("Bir adet constructor oluşturulmuştur" + a);
+        }
+
+		public MyClass() {
+
+            Console.WriteLine("Boş constructor çalıştı");
+        }
+
+		public MyClass(string a) : this()
+		{
+            Console.WriteLine(a);
+        }
+	}
+}
---
+++ DerinlemesineOOP/OOP1/_6_Constructor/Program.cs
@@ -0,0 +1,5 @@
+﻿using _6_Constructor;
+
+MyClass myClass1 = new MyClass();
+MyClass myClass2 = new MyClass(10);
+MyClass myClass3 = new MyClass("Elma");
