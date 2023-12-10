5ca39c59f36ff3bc3a813d1c999f8bf1927ad410
Fırat Alçın <firatalcin04@gmail.com>
2023-12-11 00:32:39 +0300

    Polimorfizm 1

---
+++ DerinlemesineOOP/OOP1/_8_Polimorfizm/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Polimorfizm
+{
+	public class MyClass : MyClass2
+	{
+	}
+
+	public class MyClass2
+	{
+	}
+
+
+	public class A : C
+	{
+
+	}
+
+	public class B : A
+	{
+
+	}
+
+	public class C
+	{
+
+	}
+}
---
+++ DerinlemesineOOP/OOP1/_8_Polimorfizm/Program.cs
@@ -0,0 +1,11 @@
+﻿using _8_Polimorfizm;
+
+MyClass2 myClass = new MyClass();	
+
+A a = new A();
+B b = new B();
+A c = new B();
+
+B b1 = new B();
+A b2 = new B();
+C b3 = new B();
