99ee9f0b737ef9a4822d8c79ae75645a0e3c491c
Fırat Alçın <firatalcin04@gmail.com>
2023-12-17 21:36:33 +0300

    create

---
+++ TipsOfNetDevelopers/DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context
+{
+    public sealed class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.; database=TipsForNetDevelopers; Trusted_Connection=True; Integrated Security=true;trustServerCertificate=true");
+        }
+
+        public DbSet<Product> Products { get; set; } 
+    }
+}
---
+++ TipsOfNetDevelopers/StartConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+
+namespace StartConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext db = new AppDbContext();
+            IList<Product> products = new List<Product>();
+
+
+            for (int i = 0; i < 1000000; i++) {
+
+                Product product = new Product()
+                {
+                    Name = "Product " + i
+                };
+
+                products.Add(product);
+            }
+
+            db.Products.AddRange(products);
+
+            db.SaveChanges();
+        }
+    }
+}
