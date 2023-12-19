1c83e98cc10b2fb205ed5efb8da5dca43e3b65f6
Fırat Alçın <firatalcin04@gmail.com>
2023-12-19 23:48:43 +0300

    AsNoTracking

---
+++ TipsOfNetDevelopers/AsNoTracking/BenchmarkService.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsNoTracking
+{
+    public class BenchmarkService
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default
+                    .WithRatioStyle(BenchmarkDotNet.Columns.RatioStyle.Trend);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void GetAllWithTracking()
+        {
+            AppDbContext context = new();
+            context.Products.ToList();
+        }
+
+        [Benchmark]
+        public void GetAllWithAsNoTracking()
+        {
+            AppDbContext context = new();
+            context.Products.AsNoTracking().ToList();
+        }
+
+    }
+
+}
