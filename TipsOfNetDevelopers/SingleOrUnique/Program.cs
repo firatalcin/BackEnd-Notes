2d80e84d172ebd08e58bbaf471485fc66173f772
Fırat Alçın <firatalcin04@gmail.com>
2023-12-20 23:06:19 +0300

    SingleOrUnique

---
+++ TipsOfNetDevelopers/SingleOrUnique/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+
+namespace SingleOrUnique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext context = new AppDbContext();
+
+            Product product1 = context.Products.Where(x => x.Name == "Product 0").FirstOrDefault();
+
+            Product product2 = context.Products.Where(x => x.Name == "Product 0").SingleOrDefault();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
