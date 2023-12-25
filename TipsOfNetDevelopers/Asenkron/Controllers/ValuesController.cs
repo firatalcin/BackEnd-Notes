4ad41db3ebacad687e5b817629ec4eb661a7da88
Fırat Alçın <firatalcin04@gmail.com>
2023-12-25 23:44:53 +0300

    Asenkron

---
+++ TipsOfNetDevelopers/Asenkron/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asenkron.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetFirstAsync()
+        {
+            AppDbContext context = new AppDbContext();
+
+            Product product = await context.Products.FirstOrDefaultAsync();
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public IActionResult GetFirst() 
+        {
+            AppDbContext context = new AppDbContext();
+
+            Product product = context.Products.FirstOrDefault();
+            return Ok(product);
+        }
+    }
+}
