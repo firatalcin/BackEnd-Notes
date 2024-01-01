6a5a343738f0fd448a429be7ba226583c51fbde2
Fırat Alçın <firatalcin04@gmail.com>
2024-01-01 23:04:21 +0300

    Record

---
+++ TipsOfNetDevelopers/Record/Controllers/LoginDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Record.Controllers
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public record Login(string Email, string Password, bool RememberMe = true);
+}
---
+++ TipsOfNetDevelopers/Record/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Record.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            loginDto.Email = "firatalcin@gmail.com";
+            return Ok("Giriş Başarılı");
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult LoginWithRecord(LoginDto loginDto)
+        {
+            //Record'da sonradan atama işlemi yapılamaz.
+            //Login login2 = new("firatalcin@gmail.com", "1");
+            return Ok("Giriş Başarılı");
+        }
+    }
+}
