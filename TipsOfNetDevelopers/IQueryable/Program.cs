03fe4eb5ade14e5742296e8b9d80e5be00262436
Fırat Alçın <firatalcin04@gmail.com>
2024-01-01 22:54:06 +0300

    IQueryable

---
+++ TipsOfNetDevelopers/IQueryable/Program.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+
+namespace IQueryable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext context = new AppDbContext();
+
+            IQueryable<Product> products = context.Products.AsQueryable();
+
+            IList<Product> productsList = products.Where(x => x.Id > 990000).ToList();
+        }
+    }
+}
