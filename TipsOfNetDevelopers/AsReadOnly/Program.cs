29883f856631b974aaf217d0c2994dd05edb8249
Fırat Alçın <firatalcin04@gmail.com>
2024-01-06 19:53:12 +0300

    AsReadOnly

---
+++ TipsOfNetDevelopers/AsReadOnly/Program.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+
+namespace AsReadOnly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext context = new AppDbContext();  
+            IReadOnlyList<Product> products = context.Products.ToList().AsReadOnly();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
