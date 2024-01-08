6055b93a7126b53e174d58e8eb36bcc388ad088f
Fırat Alçın <firatalcin04@gmail.com>
2024-01-08 23:36:41 +0300

    Any And All Method

---
+++ TipsOfNetDevelopers/AnyAllMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Context;
+
+namespace AnyAllMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext context = new AppDbContext();
+
+            bool result = context.Products.Any();
+
+            bool result2 = context.Products.Any(p => p.Name == "Product 1");
+
+            bool result3 = context.Products.All(p => p.Name.Contains("P"));
+
+
+        }
+    }
+}
