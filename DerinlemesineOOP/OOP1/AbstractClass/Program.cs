10dede4cd8a3ad325094eda45e835d3bdcd4944c
Fırat Alçın <firatalcin04@gmail.com>
2024-01-27 19:39:18 +0300

    Abstract Class

---
+++ DerinlemesineOOP/OOP1/AbstractClass/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace AbstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    abstract class MyAbstractClass
+    {
+        int a;
+
+        public void X()
+        {
+
+        }
+
+        public int MyProperty { get; set; }
+
+        abstract public void Z();
+        abstract public void W(string a, int b);
+        abstract public int Y { get; set; }
+        abstract public bool H();
+    }
+
+    class MyClass : MyAbstractClass
+    {
+        public override int Y { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override bool H()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void W(string a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Z()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
