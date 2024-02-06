3664cbfa62501bb7a00fc2af923532887fc022bb
Fırat Alçın <firatalcin04@gmail.com>
2024-02-06 23:41:14 +0300

    Proje Oluşturuldu

---
+++ AspNetCoreDersleri/MeetingApp/WebUI/Program.cs
@@ -0,0 +1,19 @@
+namespace WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
