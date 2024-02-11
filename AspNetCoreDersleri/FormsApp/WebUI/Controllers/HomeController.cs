db7e66a92c3b123f308d7ce32463736c734fc6eb
Fırat Alçın <firatalcin04@gmail.com>
2024-02-11 18:21:42 +0300

    Product List

--- AspNetCoreDersleri/FormsApp/WebUI/Controllers/HomeController.cs
+++ AspNetCoreDersleri/FormsApp/WebUI/Controllers/HomeController.cs
@@ -1,32 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WebUI.FakeDb;
 using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
+            return View(Repository.GetProductList());
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ AspNetCoreDersleri/FormsApp/WebUI/FakeDb/Repository.cs
@@ -0,0 +1,36 @@
+﻿using WebUI.Models;
+
+namespace WebUI.FakeDb
+{
+    public static class Repository
+    {
+        private static readonly List<Product> _products = new List<Product>();
+        private static readonly List<Category> _categories = new List<Category>();
+
+        static Repository()
+        {
+            _categories.Add(new Category { Id=1, Name = "Telefon"});
+            _categories.Add(new Category { Id = 2, Name = "Bilgisayar" });
+
+            _products.Add(new Product { Id = 1, Name = "IPhone 14", Price = 40000, IsActive = true, Image = "1.jpg", CategoryId = 1 });
+            _products.Add(new Product { Id = 2, Name = "IPhone 15", Price = 50000, IsActive = true, Image = "2.jpg", CategoryId = 1 });
+            _products.Add(new Product { Id = 3, Name = "IPhone 16", Price = 60000, IsActive = true, Image = "3.jpg", CategoryId = 1 });
+            _products.Add(new Product { Id = 4, Name = "IPhone 17", Price = 70000, IsActive = true, Image = "4.jpg", CategoryId = 1 });
+
+
+            _products.Add(new Product { Id = 5, Name = "Macbook Air", Price = 80000, IsActive = true, Image = "5.jpg", CategoryId = 2 });
+            _products.Add(new Product { Id = 6, Name = "Macbook Pro", Price = 90000, IsActive = true, Image = "6.jpg", CategoryId = 2 });
+
+        }
+
+        public static List<Product> GetProductList()
+        {
+            return _products;
+        }
+
+        public static List<Category> GetCategoryList()
+        {
+            return _categories;
+        }
+    }
+}
