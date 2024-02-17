7ee16b1fdae5705b50e9e2711801e2774c092d02
Fırat Alçın <firatalcin04@gmail.com>
2024-02-18 00:02:25 +0300

    Kayıt Ekleme

---
+++ AspNetCoreDersleri/EFCoreApp/WebUI/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebUI.Context;
+using WebUI.Data;
+
+namespace WebUI.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public StudentController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Student student)
+        {
+            _appDbContext.Students.Add(student);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");           
+        }
+    }
+}
--- AspNetCoreDersleri/EFCoreApp/WebUI/Data/Student.cs
+++ AspNetCoreDersleri/EFCoreApp/WebUI/Data/Student.cs
@@ -3,9 +3,9 @@
     public class Student
     {
         public int Id { get; set; }
-        public string Ad { get; set; }
-        public string Soyad { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
         public string Email { get; set; }
-        public string Telefon { get; set; }
+        public string Phone { get; set; }
     }
 }
