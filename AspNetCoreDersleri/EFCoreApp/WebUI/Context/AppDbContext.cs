95b6322a0efa867465e12c275553e9433219df88
Fırat Alçın <firatalcin04@gmail.com>
2024-02-17 23:42:11 +0300

    Migrations

--- AspNetCoreDersleri/EFCoreApp/WebUI/Context/AppDbContext.cs
+++ AspNetCoreDersleri/EFCoreApp/WebUI/Context/AppDbContext.cs
@@ -5,6 +5,10 @@ namespace WebUI.Context
 {
     public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Register> Registers { get; set; }
