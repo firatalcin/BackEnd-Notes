f1930cffb09de8e56ad51010aa14b0987ded4c04
Fırat Alçın <firatalcin04@gmail.com>
2024-02-21 23:09:35 +0300

    Ders 2

--- MSSQLDersleri/SqlDersleri.sql
+++ MSSQLDersleri/SqlDersleri.sql
@@ -26,3 +26,38 @@ select Adi + ' ' + SoyAdi [Personel Bilgileri] from Personeller
 
 select Adi + ' ' + CONVERT(nvarchar,IseBaslamaTarihi) from Personeller
 select Adi + ' ' + CAST(IseBaslamaTarihi as nvarchar) from Personeller
+
+-- Personeller tablosunda þehri London olan verileri listele
+
+select * from Personeller where Sehir = 'London'
+select * from Personeller where Sehir = 'London' and Unvan = 'Sales Manager'
+
+-- Between
+
+select * from Personeller where YEAR(DogumTarihi) between 1950 and 1965
+
+-- In
+
+select * from Personeller where Sehir in ('London', 'Tocoma')
+
+-- LIKE
+
+select * from Personeller where Adi like 'j%'
+select * from Personeller where Adi like '%y'
+select * from Personeller where Adi like 'r%t'
+select * from Personeller where Adi like '%an%'
+select * from Personeller where Adi like 'a_d%'
+select * from Personeller where Adi like '[nmr]%' -- ilk harfi m or n or r
+select * from Personeller where Adi like '[^a]%'
+
+-- Aggregate Fonksiyonlar
+
+select AVG(PersonelID) from Personeller
+select MAX(PersonelID) from Personeller
+select Min(PersonelID) from Personeller
+select COUNT(PersonelID) from Personeller
+select sum(NakliyeUcreti) from Satislar
+
+
+
+
