2eec1fb7a16b337fc77c77dca80a68a6f96aae5c
Fırat Alçın <firatalcin04@gmail.com>
2024-02-23 23:20:38 +0300

    Delegate

---
+++ API-MVC-Bootcamp/GerekliBilgiler/01.Delege/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.Delege
+{
+    // Delegate'ler sırasıyla methodları tetikleyen yapılardır
+    delegate void myDelegate(int sayi1, int sayi2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            myDelegate myDelegate = new myDelegate(Topla);
+            myDelegate += Carp;
+            myDelegate.Invoke(1, 2);
+
+        }
+
+        static void Topla(int number1, int number2)
+        {
+            Console.WriteLine(number1+number2);
+        }
+
+        static void Carp(int number1, int number2)
+        {
+            Console.WriteLine(number1*number2);
+        }
+    }
+}
