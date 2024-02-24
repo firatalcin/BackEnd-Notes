7039d44a9c3b7b592552840767b5577b4a3f660e
Fırat Alçın <firatalcin04@gmail.com>
2024-02-25 00:08:02 +0300

    Actions

--- API-MVC-Bootcamp/MvcBasic/MvcBasic/Controllers/HomeController.cs
+++ API-MVC-Bootcamp/MvcBasic/MvcBasic/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace MvcBasic.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             ViewBag.Name = "Fýrat";
@@ -21,18 +14,13 @@ namespace MvcBasic.Controllers
 
             Customer customer = new Customer() { Age = 25, FirstName = "Fýrat", LastName = "Alçýn"}; 
 
-            return View(customer);
-        }
+            //return View(customer);
+            return RedirectToAction("Index2");
+        }       
 
-        public IActionResult Privacy()
+        public IActionResult Index2()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
