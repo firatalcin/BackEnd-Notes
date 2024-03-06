ac506f5787f24ae84715bdf2b3db7f40b515c8d0
Fırat Alçın <firatalcin04@gmail.com>
2024-03-06 23:58:52 +0300

    IEnumerable and IQueryable

---
+++ API-MVC-Bootcamp/EntityFrameworkCoreApp/EFCore.Query/Data/Context/BlogContext.cs
@@ -0,0 +1,27 @@
+﻿using EFCore.Query.Data.Configurations;
+using EFCore.Query.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Query.Data.Context
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MAKINA\\SQLEXPRESS01; database= UdemyEFCoreQuery; integrated security=true;TrustServerCertificate=True");
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BlogConfiguration());
+        }
+    }
+}
---
+++ API-MVC-Bootcamp/EntityFrameworkCoreApp/EFCore.Query/Program.cs
@@ -0,0 +1,41 @@
+﻿using EFCore.Query.Data.Context;
+using EFCore.Query.Data.Entities;
+
+namespace EFCore.Query
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new BlogContext();
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    context.Add(new Blog
+            //    {
+            //        Title = $"Blog {i}",
+            //        Url = $"firat.com/Blog-{i}"
+            //    });
+            //}
+
+            //context.SaveChanges();
+
+
+            //IEnumerable & IQueryable
+            
+            var query = context.Blogs.AsQueryable();
+
+            var blogs1 = query.Where(x => x.Title.Contains("Blog 1") || x.Title.Contains("Blog 2")).AsEnumerable();
+
+            var blogs2 = blogs1.Where(x => x.Title.Contains("Blog 1"));
+
+            foreach (var item in blogs2)
+            {
+                Console.WriteLine(item.Title);
+            }
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
