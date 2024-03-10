f47cf7ee33cd02194dc2939f36d05b43fa8c4a72
Fırat Alçın <firatalcin04@gmail.com>
2024-03-10 19:23:28 +0300

    Gerekli Ortamın Hazırlanması

---
+++ API-MVC-Bootcamp/BankApp/BankApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using BankApp.Web.Data.Context;
+using BankApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BankApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
