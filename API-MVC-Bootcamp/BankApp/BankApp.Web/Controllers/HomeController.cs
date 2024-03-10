cd664613ed92efe072908744898e21be04a1546c
Fırat Alçın <firatalcin04@gmail.com>
2024-03-10 19:30:55 +0300

    User List

--- API-MVC-Bootcamp/BankApp/BankApp.Web/Controllers/HomeController.cs
+++ API-MVC-Bootcamp/BankApp/BankApp.Web/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace BankApp.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(_context.ApplicationUsers.ToList());
         }
     }
 }
