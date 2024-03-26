16a1d37ff9bcbd4444fc96f0506bfc028f75d82e
Fırat Alçın <firatalcin04@gmail.com>
2024-03-26 14:20:19 +0300

    DbContext

---
+++ AspNetCoreDersleri/BlogApp/BlogApp/Data/Context/BlogContext.cs
@@ -0,0 +1,18 @@
+﻿using BlogApp.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Data.Context
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
