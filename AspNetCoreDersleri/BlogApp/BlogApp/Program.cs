017685dfc2135f0a583cdbeb8d8f00d77e51b6b3
Fırat Alçın <firatalcin04@gmail.com>
2024-04-07 17:25:41 +0300

    Connection String

--- AspNetCoreDersleri/BlogApp/BlogApp/Program.cs
+++ AspNetCoreDersleri/BlogApp/BlogApp/Program.cs
@@ -1,3 +1,6 @@
+using BlogApp.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace BlogApp
 {
     public class Program
@@ -5,6 +8,11 @@ namespace BlogApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<BlogContext>(opt =>
+            {
+                opt.UseSqlite(builder.Configuration.GetConnectionString("sql_connection"));
+            });
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
