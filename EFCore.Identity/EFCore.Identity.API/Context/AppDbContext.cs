149bd8afa15bbae413596ff95f6a6b85decddf0d
Fırat Alçın <firatalcin04@gmail.com>
2024-05-04 15:19:18 +0300

    CodeFirst ile Db Oluşturna

---
+++ EFCore.Identity/EFCore.Identity.API/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFCore.Identity.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Identity.API.Context
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
