f8056df40f6ffa359fd270457e07cb56da9e5e75
Fırat Alçın <firatalcin04@gmail.com>
2024-11-22 22:55:57 +0300

    Post Listesi

---
+++ AspNetCoreDersleri/BlogApp/BlogApp.Web/Controllers/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using BlogApp.Web.Data.Concrete.EfCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Web.Controllers;
+
+public class PostsController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public PostsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET
+    public IActionResult Index()
+    {
+        return View(_context.Posts.ToList());
+    }
+}
