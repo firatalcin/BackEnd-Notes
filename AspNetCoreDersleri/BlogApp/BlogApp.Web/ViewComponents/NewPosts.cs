2e8019c1564598ace35aa43e7275ea76c0d9b1fb
Fırat Alçın <firatalcin04@gmail.com>
2024-11-24 00:36:41 +0300

    vc

--- AspNetCoreDersleri/BlogApp/BlogApp.Web/ViewComponents/NewPosts.cs
+++ AspNetCoreDersleri/BlogApp/BlogApp.Web/ViewComponents/NewPosts.cs
@@ -1,6 +1,24 @@
-﻿namespace BlogApp.Web.ViewComponents;
+﻿using BlogApp.Web.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class NewPosts
+namespace BlogApp.Web.ViewComponents;
+
+public class NewPosts : ViewComponent
 {
-    
+    private readonly IPostRepository _postRepository;
+
+    public NewPosts(IPostRepository postRepository)
+    {
+        _postRepository = postRepository;
+    }
+
+    public async Task<IViewComponentResult>  InvokeAsync()
+    {
+        return View(await _postRepository
+                .Posts
+                .OrderByDescending(p => p.PublishedOn)
+                .Take(5)
+                .ToListAsync());
+    }
 }
--- AspNetCoreDersleri/BlogApp/BlogApp.Web/ViewComponents/TagsMenu.cs
+++ AspNetCoreDersleri/BlogApp/BlogApp.Web/ViewComponents/TagsMenu.cs
@@ -1,6 +1,20 @@
-﻿namespace BlogApp.Web.ViewComponents;
+﻿using BlogApp.Web.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class TagsMenu
+namespace BlogApp.Web.ViewComponents;
+
+public class TagsMenu : ViewComponent
 {
-    
+    private readonly ITagRepository _tagRepository;
+
+    public TagsMenu(ITagRepository tagRepository)
+    {
+        _tagRepository = tagRepository;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View(await _tagRepository.Tags.ToListAsync());
+    }
 }
