bdce81bc533df9e97dfd34b001d6b5a9250097d1
Fırat Alçın <firatalcin04@gmail.com>
2024-11-27 20:52:46 +0300

    Login Sayfası

---
+++ AspNetCoreDersleri/BlogApp/BlogApp.Web/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Web.Controllers;
+
+public class UsersController : Controller
+{
+    // GET
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
