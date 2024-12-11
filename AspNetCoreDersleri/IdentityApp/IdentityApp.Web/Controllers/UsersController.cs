394b24d6aafd8cb699f7c8361457504912830a18
Fırat Alçın <firatalcin04@gmail.com>
2024-12-11 20:43:53 +0300

    Kullanıcı Ekleme

--- AspNetCoreDersleri/IdentityApp/IdentityApp.Web/Controllers/UsersController.cs
+++ AspNetCoreDersleri/IdentityApp/IdentityApp.Web/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using IdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApp.Web.Controllers;
@@ -17,5 +18,38 @@ public class UsersController : Controller
     public IActionResult Index()
     {
         return View(_userManager.Users.ToList());
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            foreach (var err in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, err.Description);
+            }
+        }
+        
+        return View(model);
     }
 }
