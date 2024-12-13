9dfd842e59dce4cd11bc03b7a90f2333b48a7351
Fırat Alçın <firatalcin04@gmail.com>
2024-12-14 00:47:48 +0300

    Login Sayfası

---
+++ AspNetCoreDersleri/IdentityApp/IdentityApp.Web/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Web.Controllers;
+
+public class AccountController : Controller
+{
+    // GET
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
