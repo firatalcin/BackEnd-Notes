ee280f1dafa91bca7ba057188d0601ca40f53c6f
Fırat Alçın <firatalcin04@gmail.com>
2024-12-19 23:29:54 +0300

    Veri Silme

--- AspNetCoreDersleri/WebAPI/ProductApi/Controllers/ProductsController.cs
+++ AspNetCoreDersleri/WebAPI/ProductApi/Controllers/ProductsController.cs
@@ -74,5 +74,34 @@ namespace ProductApi.Controllers
             
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            
+            _context.Products.Remove(product);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
     }
 }
